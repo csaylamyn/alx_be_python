a626f448faa86626fe98a3bfae207b22562ac1b5
Lamin Ceesay <csaylamyn@yahoo.com>
2025-09-09 23:59:30 +0000

    update basic_operations.py contents

--- basic_operations.py
+++ basic_operations.py
@@ -8,6 +8,6 @@ difference_result = number1 - number2
 product_result = number1 * number2
 
 # print results
-print(f"sum of {number1} and {number2} is {sum_result}")
-print(f"difference of {number1} and {number2} is {difference_result}")
-print(f"product of {number1} and {number2} is {product_result}")
+print(f"Addition       of {number1} and {number2} is {sum_result}")
+print(f"Subtraction    of {number1} and {number2} is {difference_result}")
+print(f"Multiplication of {number1} and {number2} is {product_result}")
