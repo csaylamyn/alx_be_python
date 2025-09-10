206732c88b44c277dc5d8ae48f33a45f6f60b74f
csaylamyn <csaylamyn@yahoo.com>
2025-09-10 13:31:50 +0000

    add python_introduction directory

--- basic_operations.py
+++ basic_operations.py
@@ -1,13 +1,16 @@
 #define vaiables
+
 number1 = 10
 number2 = 5
 
 # perform operations
+
 sum_result = number1 + number2
 difference_result = number1 - number2
 product_result = number1 * number2
 
 # print results
+
 print(f"Addition       of {number1} and {number2} is {sum_result}")
 print(f"Subtraction    of {number1} and {number2} is {difference_result}")
 print(f"Multiplication of {number1} and {number2} is {product_result}")
